f06201ba0b38e22070345bd022b109520bad40cf
Laura Cole <lauracole1900@comcast.net>
2023-12-30 22:11:51 -0700

    changed base to include build folder in vite.config; still trying to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/holiday-ecard-2023/',
+  base: '/holiday-ecard-2023/build',
   build: {
     outDir: 'build'
   },
