31f91a96b94c8640f39cd91e905ff52a19f256ad
Laura Cole <lauracole1900@comcast.net>
2023-12-30 22:15:54 -0700

    adjusted pathing in root index.html to include dist

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/holiday-ecard-2023/build',
-  build: {
-    outDir: 'build'
-  },
+  base: '/holiday-ecard-2023/',
   plugins: [react()],
   server: {
     port: 3000,
