0781d5557edf5e7c8054df59d2dc8cbb5b661118
Laura Cole <lauracole1900@comcast.net>
2023-12-30 21:34:53 -0700

    rebuilt, trying to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/holiday-ecard-2023',
+  base: '/holiday-ecard-2023/',
   plugins: [react()],
   server: {
     port: 3000,
