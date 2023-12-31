e89d63f815cbdd8f028f7ddaa1b66b90084f2bb0
Laura Cole <lauracole1900@comcast.net>
2023-12-30 21:51:42 -0700

    adjusted build.outDir in vite.config & rebuilt

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/holiday-ecard-2023/',
+  build: {
+    outDir: 'build'
+  },
   plugins: [react()],
   server: {
     port: 3000,
